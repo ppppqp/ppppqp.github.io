fdcf6d0b8757cd6a8f30500e426af3353dbf3680
Qiping Pan <panqiping@outlook.com>
2024-03-14 00:13:04 -0400

    feat: redirection complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()]
+  integrations: [mdx(), sitemap(), react()],
+  redirects: {
+    '/': '/tech'
+  }
 });
